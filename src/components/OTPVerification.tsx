6895ff323292f4d59bd4526505b19f56c93b687d
Harsh <thakur2004harsh@gmail.com>
2025-01-08 19:04:31 +0530

    updated

--- src/components/OTPVerification.tsx
+++ src/components/OTPVerification.tsx
@@ -29,7 +29,7 @@ export default function OTPVerification() {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    email: 'Sethims777@gmail.com'
+                    email: 'thakur2004harsh@gmail.com'
 
                 }),
             })
